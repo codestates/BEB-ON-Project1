8dcc2f9ef88ae6cb13f7d5ee924839c71a024638
JSND-OJ00 <jsnd.oj@gmail.com>
2022-08-09 21:48:59 +0900

    hmm

--- Backend/deploy/01-deploy-basic-nft.js
+++ Backend/deploy/01-deploy-basic-nft.js
@@ -1,19 +1,32 @@
-const { network, getNamedAccounts, deployments } = require("hardhat");
+const { network, getNamedAccounts, deployments, ethers } = require("hardhat");
 const { developmentChains } = require("../helper-hardhat-config");
 const { verify } = require("../utils/verify");
+const fs = require("fs");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  console.log("------------------------------");
+  console.log("---------------------");
+  console.log("Deploying Contract...");
   args = [];
-  const nftMarketplace = await deploy("BasicNft", {
+  const basicNft = await deploy("BasicNft", {
     from: deployer,
     args: args,
     log: true,
     waitConfirmations: 1,
   });
+
+  console.log("Getting Contract Address & ABI...");
+  const deployedContract = await ethers.getContract("BasicNft");
+  const contractAddress = deployedContract.address;
+  const contractAbi = deployedContract.interface.format(
+    ethers.utils.FormatTypes.json
+  );
+  console.log("Done!");
+  console.log("---------------------");
+
+  fs.readFile("../../web3-practice/tools/abi.json");
 
   //   if (
   //     !developmentChains.includes(network.name) &&
