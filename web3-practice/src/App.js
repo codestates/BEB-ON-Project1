bd018b176721de2b432476f897ee50a55e42dd4b
bcdy19 <bcdy19@gmail.com>
2022-08-10 02:24:40 +0900

    Mypage 다시

--- web3-practice/src/App.js
+++ web3-practice/src/App.js
@@ -1,17 +1,10 @@
-import React, { useState } from 'react';
-import Mypage from './Components/Mypage';
 import './App.css';
-import { BrowserRouter} from 'react-router-dom';
-import { NFTitems } from './Components/NFTList';
 
 function App() {
-  const [items, setItems] = useState(NFTitems.items);
   return (
-    <BrowserRouter>
-        <div>
-          <Mypage items={items}/>
-        </div>
-    </BrowserRouter>
+    <div className="App">
+      
+    </div>
   );
 }
 
