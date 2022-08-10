3a11c9d22759f0e3a82984fd0d6a9f9d64c802de
JSND-OJ00 <jsnd.oj@gmail.com>
2022-08-10 14:01:30 +0900

    update constants

---
+++ web3-practice/src/constants/index.js
@@ -0,0 +1,11 @@
+const tokenAbi = require("./tokenAbi.json");
+const tokenAddress = require("./tokenAddress.json");
+const marketplaceAbi = require("./marketplaceAbi.json");
+const marketplaceAddress = require("./marketplaceAddress.json");
+
+module.exports = {
+  tokenAbi,
+  tokenAddress,
+  marketplaceAbi,
+  marketplaceAddress,
+};
