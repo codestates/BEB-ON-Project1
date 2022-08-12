1e7dea0fabbc1fe5a24d105459ca165adac273bc
dhkim <rlawkdgks111@naver.com>
2022-08-12 16:22:55 +0900

    update app.js

--- web3-practice/src/App.js
+++ web3-practice/src/App.js
@@ -1,27 +1,4 @@
 import './App.css';
-import React, {useState, useEffect} from 'react';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Mypage from './Components/Mypage';
-import NFTdetail from './Components/NFTdetail';
-//import { NFTitems } from './Components/NFTList';
-//import Nav from './Components/Nav';
-
-
-function App() {
-
-    
-    return (
-        <BrowserRouter>
-         <Routes>
-             <Route path="/" element={<Mypage />} />
-             <Route path="/nftdetail" element={<NFTdetail />} />
-         </Routes>
-        </BrowserRouter>
-    );
-}
-
-export default App;
-/*import './App.css';
 import Nav from './Components/Nav';
 import Home from './Components/Home';
 import Footer from './Components/Footer';
@@ -35,6 +12,8 @@ import ProceedManage from './Components/ProceedManage';
 
 import {useState, useEffect} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Mypage from './Components/Mypage';
+import NFTdetail from './Components/NFTdetail';
 
 function App() {
   const [mainweb3, setMainweb3] = useState("");
@@ -78,11 +57,12 @@ function App() {
             <Route path="/mypage" 
                 element={<Mypage  
                  />} /> 
-         
+             <Route path="/nftdetail" element={<NFTdetail />} />
+             
         </Routes>
         <div><Footer /></div>
        </BrowserRouter>
     );
 }
 
-export default App;*/
+export default App;
