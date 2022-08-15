573c0c4819f517b4435760a05624724a41e9005f
Jungheon Oh <103742251+JSND-OJ00@users.noreply.github.com>
2022-08-16 06:18:27 +0900

    minor changes
    업로드 파일 형식 jpeg, png, gif로 제한.
    NFT_STORAGE_TOKEN 환경변수로.

--- web3-practice/src/Pages/MintNft.jsx
+++ web3-practice/src/Pages/MintNft.jsx
@@ -4,10 +4,11 @@ import { NFTStorage, File } from "nft.storage/dist/bundle.esm.min.js";
 import { getDatabase, ref, set } from "firebase/database";
 import Web3 from "web3";
 
-export default function MintNft({account, web3}) {
+export default function MintNft({ account, web3 }) {
   // const web3 = new Web3(window.ethereum);
   // const account = "0x39a475635b9D73e7dD8336B035781d0C51Ec367a";
   const tokenAddr = tokenAddress[0];
+  const NFT_STORAGE_TOKEN = process.env.NFT_STORAGE_TOKEN;
   let tokenUri;
   let ImgURL = "";
 
@@ -34,19 +35,23 @@ export default function MintNft({account, web3}) {
 
   const setMetadata = async () => {
     try {
-      const NFT_STORAGE_TOKEN =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDRBNmU0NzdmRDdmMzBlNWI5QzBjOWM0ODkwQjhiODBGYWQwMjhlRDEiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2MDI1NDk1Mzg1OCwibmFtZSI6ImsybzIifQ.s61x9e8Xma1YakcTBsdTxrTFiu1sGhIpiXYsdMWjDWo";
       const client = new NFTStorage({ token: NFT_STORAGE_TOKEN });
       const metadata = await client.store({
         name: tokenName,
         description: "",
-        image: new File([file], tokenName, { type: "image/png" }),
+        image: new File(
+          [file],
+          tokenName,
+          { type: "image/png" } || { type: "image/jpeg" } || {
+              type: "image/gif",
+            }
+        ),
       });
       console.log(metadata.url);
       tokenUri = metadata.url.slice(7);
       // 사진이 바로 보이게 하기 위해 추가
       ImgURL = await getImgURL();
-      ImgURL = "https://ipfs.io/ipfs" + ImgURL ;
+      ImgURL = "https://ipfs.io/ipfs" + ImgURL;
     } catch (err) {
       console.error(err);
     }
@@ -54,12 +59,12 @@ export default function MintNft({account, web3}) {
 
   // 사진이 바로 보이게 하기 위해 추가
   const getImgURL = () => {
-    return new Promise(function (receive){
-      fetch("https://ipfs.io/ipfs/"+tokenUri)
-      .then(response =>response.json())
-      .then(data => receive(data["image"].slice(6)));
+    return new Promise(function (receive) {
+      fetch("https://ipfs.io/ipfs/" + tokenUri)
+        .then((response) => response.json())
+        .then((data) => receive(data["image"].slice(6)));
     });
-  }
+  };
 
   const dbUpload = (ti) => {
     try {
@@ -111,7 +116,11 @@ export default function MintNft({account, web3}) {
     <div>
       <label>
         Upload Image
-        <input type="file" onChange={retrieveFile} />
+        <input
+          type="file"
+          accept="image/png, image/jpeg, image/gif"
+          onChange={retrieveFile}
+        />
       </label>
       <label>
         Token Name
